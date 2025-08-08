b84845b74fc3fdfb295e7a6a6558a16dc7e892b9
Maria Diederich <mdiederi@c1r9s3.42luxembourg.lu>
2025-08-08 06:36:32 +0200

    rm exit status "builtin" as $? is already expanded to exit status in parsing

--- src/mini_handle_built_in.c
+++ src/mini_handle_built_in.c
@@ -22,31 +22,28 @@ int	is_builtin(char *cmd)
 	ft_strcmp(cmd, "export") == 0 || 
 	ft_strcmp(cmd, "unset") == 0 || 
 	ft_strcmp(cmd, "env") == 0 || 
-	ft_strcmp(cmd, "exit") == 0 ||
-	ft_strcmp(cmd, "$?") == 0)
+	ft_strcmp(cmd, "exit") == 0)
 		return (1);
 	return (0);
 }
 
 int	handle_builtin(t_mini *mini)	//is going to change
 {
-	if (!mini->cmdargs || !mini->args[0])
+	if (!mini->cur_cmd->args || !mini->cur_cmd->args[0])
 		return (1);
-	if (ft_strcmp(mini->args[0], "echo") == 0)
-		return (mini_echo(mini->args));
-	else if (ft_strcmp(mini->args[0], "cd") == 0)
+	if (ft_strcmp(mini->cur_cmd->args[0], "echo") == 0)
+		return (mini_echo(mini->cur_cmd->args));
+	else if (ft_strcmp(mini->cur_cmd->args[0], "cd") == 0)
 		return (mini_cd(mini));
-	else if (ft_strcmp(mini->args[0], "pwd") == 0)
+	else if (ft_strcmp(mini->cur_cmd->args[0], "pwd") == 0)
 		return (mini_pwd());
-	else if (ft_strcmp(mini->args[0], "export") == 0)
+	else if (ft_strcmp(mini->cur_cmd->args[0], "export") == 0)
 		return (mini_export(mini));
-	else if (ft_strcmp(mini->args[0], "unset")== 0)
+	else if (ft_strcmp(mini->cur_cmd->args[0], "unset")== 0)
 		return (mini_unset(mini));
-	else if (ft_strcmp(mini->args[0], "env") == 0)
+	else if (ft_strcmp(mini->cur_cmd->args[0], "env") == 0)
 		return (mini_env(&mini->env_struct));
-	else if (ft_strcmp(mini->args[0], "exit") == 0)
+	else if (ft_strcmp(mini->cur_cmd->args[0], "exit") == 0)
 		return (mini_exit(mini));
-	else if (ft_strcmp(mini->args[0], "$?") == 0)
-		return (mini->exit_status);
 	return (127);
 }
